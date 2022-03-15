61aead836f463b29cfa3b6fd315e9fbbb51f5c15
10015568 <10015568@sbstudents.org>
2022-03-15 14:04:30 -0400

    Quotient functionality

--- VRGitDemo.java
+++ VRGitDemo.java
@@ -1,6 +1,14 @@
+interface Quotient //interface for quotient
+{
+	void quo(float a, float b);
+}
+
 public class VRGitDemo
 {
 	public static void main(String[] args)
 	{
+		//QUOTIENT
+		Quotient q = (float a, float b) -> System.out.printf("%.3f", a/b);
+		q.quo(5f, 2f);
 	}
 }
